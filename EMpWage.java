c9968073eb4bb3f52b4ea54132d2073226cecdad
Raj <rajkamal11126@gmail.com>
2020-10-17 00:39:01 +0530

    added method to compute empwage

--- EMpWage.java
+++ EMpWage.java
@@ -1,38 +1,52 @@
 //Welcome to Employee Wage Simulation 
 public class EMpWage
 {
-	static final int isFullTime=1;
-	static final int isPartTime=2;
-	static final int workingDays=20;
-	static final int maxHrsInMonth=100;
-	static int totalHours=0;
-	static final double randomValue=Math.floor(Math.random()*10)%2;
-	static final int empRatePerHr=20;
-	static int salary;
-	static int empHrs;
+	private static final int EMP_RATE_PER_HRS = 20;
+	private static final int NUM_OF_WORKING_DAYS = 20;
+	private static final int MAX_HRS_IN_MONTH = 100;
 
-	public static void getEmpHrs()
-	{
-	while (totalHours<maxHrsInMonth && totalWorkingDays < workingDays ){
-		totalWorkingDays++;
-		
-		switch((int)randomValue)
-		{
-			case 1:
-				empHrs=8;
-				break;
-			case 2:
-            empHrs=4;
-				break;
-         default :
-				empHrs=0;
-      }
-		totalHours=totalHours+empHrs;
-		totalSalary=empRatePerHr*totalHours;
+	public static void main(String[] args) {
+		calDailyEmpWage();
 	}
-	System.out.println("Total Salary="+totalSalary);
-	public static void main(String args[])
-	{
-		getEmpHrs();
+
+	// calculate employee daily wages based on type of employee 
+	public static void calDailyEmpWage() {
+		int totalWage = 0;
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 0;
+		while(totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS){
+			totalWorkingDays++;
+
+			final int empHrs = getEmpHrs();
+			final int empWage = empHrs*EMP_RATE_PER_HRS;
+			totalEmpHrs+=empHrs;
+			totalWage+=empWage;
+			System.out.println("Emp DAY : "+totalWorkingDays+" wages : "+empWage);
+		}
+		System.out.println("Total emp wage : "+ totalWage);
+	}
+	public static int getEmpHrs() {
+
+		final int isFullTime = 1;
+		final int isPartTime = 2;
+		int empHrs = 0;
+
+		final double randomValue = Math.floor(Math.random()*10)%3;
+
+		switch((int)randomValue) {
+
+			case isFullTime:
+				empHrs = 8;
+				System.out.println("Emp is present for full time.");
+				break;
+			case isPartTime:
+				empHrs = 4;
+				System.out.println("Emp is present for part time.");
+				break;
+			default:
+				System.out.println("Emp is absent");
+				break;
+		}
+		return empHrs;
 	}
 }
