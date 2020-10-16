6dc641a584e0d1a75af90e7b89af7070d8dea521
Raj <rajkamal11126@gmail.com>
2020-10-16 12:24:51 +0530

    added monthly working days

--- EMpWage.java
+++ EMpWage.java
@@ -3,6 +3,7 @@ public class EMpWage
 {
 	static final int isFullTime=1;
 	static final int isPartTime=2;
+	static int workingDays=20;
 	static final double randomValue=Math.floor(Math.random()*10)%2;
 	static final int empRatePerHr=20;
 	static int salary;
@@ -10,19 +11,22 @@ public class EMpWage
 
 	public static void getEmpHrs()
 	{
-		switch((int)randomValue)
-		{
-      	case 1:
+		for(int i=0; i<=workingDays; i++){
+			switch(randomValue)
+			{
+			case 1:
 				empHrs=8;
 				break;
 			case 2:
             empHrs=4;
 				break;
-       	default :
+         default :
 				empHrs=0;
-        }
-	salary=empRatePerHr*empHrs;
-	System.out.println("salary= "+salary);
+			}
+		salary=empRatePerHr*empHrs;
+		totalSalary=totalSalary+salary;
+	}
+		System.out.println("Total Salary="+totalSalary);
 	}
 	public static void main(String args[])
 	{
