7b753e29da39f101ab1e50b149db2b8e6a11d3ef
Raj <rajkamal11126@gmail.com>
2020-10-16 11:48:45 +0530

    CheckAttendance

--- EMpWage.java
+++ EMpWage.java
@@ -1,1 +1,21 @@
-Welcome to Employee Wage Simulation 
+//Welcome to Employee Wage Simulation 
+public class EmpWage
+{
+	static final int isFullTime=1;
+	public static void Attendance()
+	{
+		double randomValue=Math.floor(Math.random()*10)%2;
+		if(randomValue == isFullTime)
+		{
+			System.out.println("Employee is present");
+		}
+		else
+		{
+			System.out.println("Employee is absent");
+		}
+	}
+	public static void main(String args[])
+	{
+		Attendance();
+	}
+}
